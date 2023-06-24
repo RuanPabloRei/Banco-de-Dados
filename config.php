5b087d5940ed5274fdc5d9f9a41c38bdb4baa2a8
RuanPabloRei <132849002+RuanPabloRei@users.noreply.github.com>
2023-06-24 15:32:22 -0300

    Upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'formular-rua';
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName)
+
+    if($conexao-> connect_errno)
+    {
+        echo "Erro";
+    }
+
+    else
+    {
+        echo "conexão efetuada com sucesso"
+    }
+?>
